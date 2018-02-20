892707fb7666dc5954bceb4b07780e4602f772a8
Anju Dubey <anju_dubey@apple.com>
2018-02-19 17:09:04 -0800

    flattenedArray.js

---
+++ flattenedArray.js
@@ -0,0 +1,19 @@
+// write a function that takes an array as input that can contain both ints and more arrays (which can also contain an array or int) and return the flattened array.
+// ex. [1, [2, [ [3, 4], 5], 6]] =&gt; [1, 2, 3, 4, 5, 6]
+
+
+function flattern(arr, output) {
+	if (!arr) return;
+
+	arr.forEach(item => {
+		if (Array.isArray(item)) {
+			flattern(item, output);
+		} else {
+			output.push(item);
+		}
+	});
+
+	return output;
+}
+
+console.info(flattern([1, [2, [ [3, 4], 5], 6]], []));
